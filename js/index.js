d700015b359623773e7a098fdb7bff75a858c124
Aswin K R <aswinkr77@gmail.com>
2021-06-30 20:46:42 +0530

    final changes

--- js/index.js
+++ js/index.js
@@ -12,7 +12,6 @@ let start = false, stop = false, i = 3, generateObstaclesTimer, points = 0, cTop
 let pointsCounter = () => {
     if(!stop) {
         ++points;
-
         if(points < 10) 
             point.innerHTML = `000${points} Pts`;
         else if(points < 100)
@@ -31,9 +30,7 @@ let pointsCounter = () => {
 // remove jump ability of captain finn and also set gameover captain finn
 let removeJump = () => {
     captainFinn.classList.remove("jump");
-    if(stop) 
-        captainFinn.src = "assets/images/captain finn/6.png";
-    else {
+    if(!stop) {
         captainFinn.src = "assets/images/captain finn/1.png";
         i = 3;
     }
@@ -52,9 +49,8 @@ let removeCloudAnimation = () => {
 
 // add jump ability to captain and also set jump image of captain finn
 let jump = () => {
-    if (captainFinn.classList != "jump") {
+    if(captainFinn.classList != "jump") {
         captainFinn.classList.add("jump");
-        captainFinn.src = "assets/images/captain finn/2.png"
         setTimeout(removeJump, 1000 * 0.7);
     }
 }
@@ -68,7 +64,7 @@ let removeObstaclesMotion = () => {
 let generateObstacles = () => {
     let randInt;
     if(obstacles.classList != "obstacles-move" && !stop) {
-        randInt = Math.floor(Math.random() * 3 + 1);
+        randInt = Math.floor(Math.random() * 3 + 1); //creates a random number between [1, 3]
         obstacles.src = `assets/images/obstacles/${randInt}.png`;
         obstacles.classList.add("obstacles-move");
         setTimeout(removeObstaclesMotion, 1000 * 1.4);
@@ -92,23 +88,25 @@ let collisionCheck = () => {
         obstacles.style.left = `${oLeft}px`;
         captainFinn.style.top = `${cTop}px`;
         wordCloud.style.left = `${parseInt(getComputedStyle(wordCloud).left)}px`;
+        captainFinn.src = "assets/images/captain finn/6.png";
         removeCloudAnimation();
         clearInterval(collided);
         clearTimeout(generateObstaclesTimer);
     } 
-    
+
     move += 10;
     if(move % 100 === 0) {
         if(i > 7)
             i = 1;
 
         if(!(cRight <= 380)) //pauses captain finn at 380px from the right
-            captainFinn.style.left = `${(move / 1000) * 5 + 1}px`;
+            captainFinn.style.left = `${(move / 1000) * 5 + 1}px`; //moves captain finn forward
 
-        if(cBottom === 0) {
+        if(cBottom === 0) { //sets different walking styles of captain finn when not jumping
             captainFinn.src = `assets/images/captain finn/${i}.png`;
             i += 2;
-        }
+        } else // sets jump image of captain finn 
+            captainFinn.src = "assets/images/captain finn/2.png";
     }
 }
 
@@ -136,11 +134,15 @@ document.addEventListener("keydown", (event) => {
         }
 
         start = true;
+    } else {
+        document.getElementsByClassName("container")[0].style.display = "none";
+        gameOver.style.display = "none";
+        document.getElementById("no-key-support").style.display = "block";
     }
 });
 
 //restart game
-gameOver.addEventListener("click", () => {
+restart.addEventListener("click", () => {
     gameOver.style.display = "none";
     point.innerHTML = "0000 Pts";
     spaceJump.style.visibility = "visible";
